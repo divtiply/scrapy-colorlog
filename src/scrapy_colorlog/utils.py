2001c4520b633b69188d58ab29c28c1b23abb4b0
Michel Ace <divtiply@gmail.com>
2024-04-17 17:41:14 +0200

    refactor: utils

--- src/scrapy_colorlog/utils.py
+++ src/scrapy_colorlog/utils.py
@@ -1,9 +1,33 @@
+from __future__ import annotations
+
 import logging
 
+import scrapy.utils.log
 from scrapy.settings import Settings
 from scrapy.utils.log import get_scrapy_root_handler
 
 from .formatter import ColoredFormatter
+
+
+def install() -> None:
+    from scrapy import crawler
+
+    crawler.configure_logging = configure_logging
+    crawler.install_scrapy_root_handler = install_scrapy_root_handler
+
+
+def configure_logging(
+    settings: Settings | dict | None = None, install_root_handler: bool = True
+) -> None:
+    if isinstance(settings, dict) or settings is None:
+        settings = Settings(settings)
+    scrapy.utils.log.configure_logging(settings, install_root_handler)
+    replace_scrapy_root_handler_formatter(settings)
+
+
+def install_scrapy_root_handler(settings: Settings) -> None:
+    scrapy.utils.log.install_scrapy_root_handler(settings)
+    replace_scrapy_root_handler_formatter(settings)
 
 
 def replace_scrapy_root_handler_formatter(settings: Settings) -> None:
@@ -16,19 +40,3 @@ def replace_scrapy_root_handler_formatter(settings: Settings) -> None:
         formatter = ColoredFormatter.from_settings(settings)
         formatter.stream = handler.stream
         handler.setFormatter(formatter)
-
-
-def install() -> None:
-    from scrapy import crawler
-    from scrapy.utils import log
-
-    def configure_logging(settings: Settings, install_root_handler: bool = True):
-        log.configure_logging(settings, install_root_handler)
-        replace_scrapy_root_handler_formatter(settings)
-
-    def install_scrapy_root_handler(settings: Settings):
-        log.install_scrapy_root_handler(settings)
-        replace_scrapy_root_handler_formatter(settings)
-
-    crawler.configure_logging = configure_logging
-    crawler.install_scrapy_root_handler = install_scrapy_root_handler
